a68951ed2a1f12997786fec996958524b41b9111
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-02-26 10:50:48 -0600

    Added JavaScript

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
+/*
+if (topic === 'HTML') {
+    console.log("Lets study HTML!");
+} else if (topic === 'css') {
+    console.log("Lets study CSS!");
+} else if (topc === 'Git') {
+    console.log("Lets study Git");
+} else if (topic === 'JavaScript') {
+    console.log("Lets study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
